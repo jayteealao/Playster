d257cfbddf4cd808ddd49c073304f7257a57239e
jayteealao <jayteealao@gmail.com>
2026-07-14 01:13:23 +0100

    build: bump version to 0.2.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.github.jayteealao.playster"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
